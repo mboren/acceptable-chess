39551e219ccba9ef1eff19f3e028ac43698ef544
Marty Boren <martyboren@gmail.com>
2020-05-08 12:56:59 -0700

    mix new output

---
+++ lib/chess_server.ex
@@ -0,0 +1,18 @@
+defmodule ChessServer do
+  @moduledoc """
+  Documentation for `ChessServer`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ChessServer.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
