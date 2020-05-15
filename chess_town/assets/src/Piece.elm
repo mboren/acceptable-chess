beed21a9aa463b5419bfa22de8a66689987a5c08
Marty Boren <martyboren@gmail.com>
2020-05-15 11:14:53 -0700

    Create Piece type
    Represents a chess piece as a combination of kind (knight, pawn, etc)
    and color (white, black)

---
+++ chess_town/assets/src/Piece.elm
@@ -0,0 +1,101 @@
+module Piece exposing (..)
+
+import Player exposing (Player(..))
+
+
+type PieceKind
+    = Pawn
+    | Rook
+    | Knight
+    | Bishop
+    | King
+    | Queen
+
+
+type alias Piece =
+    { kind : PieceKind
+    , player : Player
+    }
+
+
+fromChar : Char -> Maybe Piece
+fromChar c =
+    case c of
+        'p' ->
+            Just (Piece Pawn Black)
+
+        'r' ->
+            Just (Piece Rook Black)
+
+        'n' ->
+            Just (Piece Knight Black)
+
+        'b' ->
+            Just (Piece Bishop Black)
+
+        'k' ->
+            Just (Piece King Black)
+
+        'q' ->
+            Just (Piece Queen Black)
+
+        'P' ->
+            Just (Piece Pawn White)
+
+        'R' ->
+            Just (Piece Rook White)
+
+        'N' ->
+            Just (Piece Knight White)
+
+        'B' ->
+            Just (Piece Bishop White)
+
+        'K' ->
+            Just (Piece King White)
+
+        'Q' ->
+            Just (Piece Queen White)
+
+        _ ->
+            Nothing
+
+
+toString : Piece -> String
+toString { kind, player } =
+    case ( kind, player ) of
+        ( Pawn, Black ) ->
+            "p"
+
+        ( Rook, Black ) ->
+            "r"
+
+        ( Knight, Black ) ->
+            "n"
+
+        ( Bishop, Black ) ->
+            "b"
+
+        ( King, Black ) ->
+            "k"
+
+        ( Queen, Black ) ->
+            "q"
+
+        ( Pawn, White ) ->
+            "P"
+
+        ( Rook, White ) ->
+            "R"
+
+        ( Knight, White ) ->
+            "N"
+
+        ( Bishop, White ) ->
+            "B"
+
+        ( King, White ) ->
+            "K"
+
+        ( Queen, White ) ->
+            "Q"
