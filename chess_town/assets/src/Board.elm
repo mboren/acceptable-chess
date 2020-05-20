cfb005c154e028df9ecd576fb9767f679480dbac
Marty Boren <martyboren@gmail.com>
2020-05-20 15:12:55 -0700

    Hardcode reversal of list

--- chess_town/assets/src/Board.elm
+++ chess_town/assets/src/Board.elm
@@ -24,7 +24,7 @@ draw board ( selectablePieceSquares, selectPieceEvent ) ( selectableMoveSquares,
             [ "a", "b", "c", "d", "e", "f", "g", "h" ]
 
         ranks =
-            List.reverse [ "1", "2", "3", "4", "5", "6", "7", "8" ]
+            [ "8", "7", "6", "5", "4", "3", "2", "1" ]
 
         concatEach row =
             List.map (\f -> f ++ row) files
