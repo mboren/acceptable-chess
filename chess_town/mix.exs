bfc75e763deb36ffc25fa5236d633632c0265c16
Marty Boren <martyboren@gmail.com>
2020-05-27 11:48:48 -0700

    Add dialyzer and credo to phoenix project deps

--- chess_town/mix.exs
+++ chess_town/mix.exs
@@ -39,6 +39,8 @@ defmodule ChessTown.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
 
       {:chess_app, [path: "../chess_server"]},
     ]
