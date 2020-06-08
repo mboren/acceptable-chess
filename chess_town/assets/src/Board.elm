75032cca349edb4ebbb22c2a050cb264abca6a65
Marty Boren <martyboren@gmail.com>
2020-06-08 11:29:35 -0700

    Move light/dark square logic to Square module
    I'm also changing from white/black to light/dark to avoid
    confusion with player colors.

--- chess_town/assets/src/Board.elm
+++ chess_town/assets/src/Board.elm
@@ -61,14 +61,6 @@ replaceNumbers processed unprocessed =
                 replaceNumbers (processed ++ String.fromChar head) tail
 
 
-whiteSquares =
-    Set.fromList [ "e6", "c8", "h7", "c4", "b1", "h3", "d7", "f3", "b7", "f1", "a4", "b3", "c6", "e4", "b5", "h1", "d1", "d3", "f7", "e8", "g6", "g4", "g8", "a6", "c2", "d5", "g2", "a2", "e2", "a8", "f5", "h5" ]
-
-
-blackSquares =
-    Set.fromList [ "e7", "c7", "d8", "a5", "a7", "e1", "f4", "d2", "g1", "h8", "e3", "g5", "g7", "a3", "b8", "h4", "f8", "e5", "h2", "f2", "a1", "b4", "d4", "c3", "h6", "g3", "c1", "f6", "c5", "b2", "d6", "b6" ]
-
-
 draw : Board -> ( Set Square, Square -> msg ) -> ( Set Square, Square -> msg ) -> Player -> Element msg
 draw board ( selectablePieceSquares, selectPieceEvent ) ( selectableMoveSquares, selectMoveEvent ) currentPlayer =
     let
@@ -110,7 +102,7 @@ drawSquare : Element.Length -> ( Set Square, Square -> msg ) -> ( Set Square, Sq
 drawSquare size ( selectablePieceSquares, selectPieceEvent ) ( selectableMoveSquares, selectMoveEvent ) maybePiece square =
     let
         color =
-            if Set.member square whiteSquares then
+            if Square.isLight square then
                 Element.rgb255 237 238 210
 
             else
--- chess_town/assets/src/Square.elm
+++ chess_town/assets/src/Square.elm
@@ -1,5 +1,19 @@
 module Square exposing (..)
 
+import Set
+
 
 type alias Square =
     String
+
+
+lightSquares =
+    Set.fromList [ "e6", "c8", "h7", "c4", "b1", "h3", "d7", "f3", "b7", "f1", "a4", "b3", "c6", "e4", "b5", "h1", "d1", "d3", "f7", "e8", "g6", "g4", "g8", "a6", "c2", "d5", "g2", "a2", "e2", "a8", "f5", "h5" ]
+
+
+darkSquares =
+    Set.fromList [ "e7", "c7", "d8", "a5", "a7", "e1", "f4", "d2", "g1", "h8", "e3", "g5", "g7", "a3", "b8", "h4", "f8", "e5", "h2", "f2", "a1", "b4", "d4", "c3", "h6", "g3", "c1", "f6", "c5", "b2", "d6", "b6" ]
+
+
+isLight square =
+    Set.member square lightSquares
